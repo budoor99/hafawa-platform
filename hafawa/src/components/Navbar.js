b786a9a82387e61c0159fd46bb1611c58289491a
Malath <malathalhashim@gmail.com>
2025-04-17 15:18:20 +0300

    modifying nav bar

--- hafawa/src/components/Navbar.js
+++ hafawa/src/components/Navbar.js
@@ -57,7 +57,7 @@ function Navbar() {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/hosts">
+                <Link className="nav-link" to="/host">
                   Host
                 </Link>
               </li>
