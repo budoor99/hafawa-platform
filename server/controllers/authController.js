7d1e5b351ea78908d3df266671b7f366b11bf52f
Budoor <bedx999@example.com>
2025-05-01 14:15:58 +0300

    Add Routes and Controllers for host and guide

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,30 +1,27 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const createUser = require("../utils/createUser");
 
 exports.signup = async (req, res) => {
   try {
-    const { name, email, phone, password } = req.body;
-
-    // check if user exists
-    const existing = await User.findOne({ email });
-    if (existing)
-      return res.status(400).json({ message: "Email already exists" });
-
-    // hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // create new user
-    const user = new User({
+    const {
       name,
       email,
       phone,
-      password: hashedPassword,
-      role: "user",
-      isVerified: true, // default for now
-    });
+      password,
+      role = "user",
+      isVerified = true,
+    } = req.body;
 
-    await user.save();
+    const userId = await createUser({
+      name,
+      email,
+      phone,
+      password,
+      role,
+      isVerified,
+    });
 
     res.status(201).json({ message: "User created successfully" });
   } catch (err) {
---
+++ server/controllers/tourGuideController.js
@@ -0,0 +1,41 @@
+const bcrypt = require("bcryptjs");
+const createUser = require("../utils/createUser");
+const TourGuideProfile = require("../models/TourGuideProfile");
+
+exports.applyAsTourGuide = async (req, res) => {
+  try {
+    const { name, email, phone, password, aboutMe, city, experienceYears } =
+      req.body;
+
+    const userId = await createUser({
+      name,
+      email,
+      phone,
+      password,
+      role: "tourguide",
+      isVerified: false,
+    });
+
+    // create profile
+    const profile = new TourGuideProfile({
+      userId,
+      aboutMe,
+      languages,
+      city,
+      calendarUrl,
+      specialRequests,
+      experienceYears,
+    });
+
+    await profile.save();
+
+    res.status(201).json({
+      message: "Tour guide application submitted.",
+      userId,
+      profileId: profile._id,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
