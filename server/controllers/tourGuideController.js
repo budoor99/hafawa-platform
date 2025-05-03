476a122c30f9c60523f093ea05abbca2c0cef5d3
Malath Alhashem <118542256+IamMalath@users.noreply.github.com>
2025-05-03 15:15:38 +0300

    Update tourGuideController.js

--- server/controllers/tourGuideController.js
+++ server/controllers/tourGuideController.js
@@ -1,11 +1,11 @@
 const bcrypt = require("bcryptjs");
 const createUser = require("../utils/createUser");
 const TourGuideProfile = require("../models/TourGuideProfile");
+const User = require("../models/User");
 
 exports.applyAsTourGuide = async (req, res) => {
   try {
-    const { name, email, phone, password, aboutMe, city, experienceYears } =
-      req.body;
+    const { name, email, phone, password, aboutMe, city, experienceYears, languages, calendarUrl, specialRequests } = req.body;
 
     const userId = await createUser({
       name,
@@ -16,7 +16,6 @@ exports.applyAsTourGuide = async (req, res) => {
       isVerified: false,
     });
 
-    // create profile
     const profile = new TourGuideProfile({
       userId,
       aboutMe,
@@ -25,17 +24,74 @@ exports.applyAsTourGuide = async (req, res) => {
       calendarUrl,
       specialRequests,
       experienceYears,
+      activities
+      
     });
 
     await profile.save();
 
-    res.status(201).json({
-      message: "Tour guide application submitted.",
-      userId,
-      profileId: profile._id,
-    });
+    res.status(201).json({ message: "Tour guide application submitted.", userId, profileId: profile._id });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
   }
 };
+
+exports.getVerifiedTourGuides = async (req, res) => {
+  try {
+    const users = await User.find({ role: "tourguide", isVerified: true });
+
+    const guidesWithCity = await Promise.all(
+      users.map(async (user) => {
+        const profile = await TourGuideProfile.findOne({ userId: user._id });
+        return {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          city: profile?.city || "Unknown",
+        };
+      })
+    );
+
+    res.status(200).json(guidesWithCity);
+  } catch (err) {
+    console.error("Error fetching verified guides:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.getTourGuideProfile = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const profile = await TourGuideProfile.findOne({ userId }).populate("userId").populate("activities");;
+
+    if (!profile) {
+      return res.status(404).json({ message: "Tour guide profile not found" });
+    }
+
+    res.status(200).json({
+      user: {
+        name: profile.userId.name,
+        email: profile.userId.email,
+      },
+      city: profile.city,
+      aboutMe: profile.aboutMe,
+      experienceYears: profile.experienceYears,
+      languages: profile.languages,
+      calendarUrl: profile.calendarUrl,
+      specialRequests: profile.specialRequests,
+      activities:profile.activities
+      
+    });
+  } catch (err) {
+    console.error("Error fetching tour guide profile:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+
+
+
+
